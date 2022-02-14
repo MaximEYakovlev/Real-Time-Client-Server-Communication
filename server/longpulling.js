e59470334ae8657f4fc8f8d33deac560081fb2a5
Maxim Yakovlev <maxim.e.yakovlev@gmail.com>
2022-02-14 21:28:16 +0300

    server is running...

---
+++ server/longpulling.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const PORT = process.env.PORT || 5001;
+
+const app = express();
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
