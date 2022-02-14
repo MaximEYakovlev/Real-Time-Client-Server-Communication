a6926ee77649fcf60771f809782e46ef6edfce77
Maxim Yakovlev <maxim.e.yakovlev@gmail.com>
2022-02-14 21:38:56 +0300

    created EventEmitter

--- server/longpulling.js
+++ server/longpulling.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const cors = require("cors");
+const events = require("events");
 require("dotenv").config();
 
 const PORT = process.env.PORT || 5001;
 
+const emitter = new events.EventEmitter();
+
 const app = express();
 
 app.use(cors());
+
+app.get("/get-massages", (req, res) => {});
+
+app.post("/new-messages", (req, res) => {});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
