84a60adf59d078536c6b2118052c9b0cc2662518
Maxim Yakovlev <maxim.e.yakovlev@gmail.com>
2022-02-14 21:53:45 +0300

    server: the Long Polling logic is implemented

--- server/longPolling.js
+++ server/longPolling.js
@@ -11,9 +11,17 @@ const app = express();
 
 app.use(cors());
 
-app.get("/get-massages", (req, res) => {});
+app.get("/get-massages", (req, res) => {
+  emitter.once("newMessage", (message) => {
+    res.json(message);
+  });
+});
 
-app.post("/new-messages", (req, res) => {});
+app.post("/new-messages", (req, res) => {
+  const message = req.body;
+  emitter.emit("newMessage", message);
+  res.status(200);
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
